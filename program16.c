fa853be854ba023b8259faf5eb79eb4153a6e145
tharanikrish <31505996+tharanikrish@users.noreply.github.com>
2018-01-21 12:03:46 +0530

    Update program16.c

--- program16.c
+++ program16.c
@@ -1,20 +1,22 @@
 #include<stdio.h>
 int main()
 {
-int a,b,c,i,flag=0;
+int a,b,i,flag;
 printf("enter the numbers:\n");
 scanf("%d%d",&a,&b);
 while(a<b)
 {
-for(i=2;i<=b;++i)
+flag=0;
+for(i=2;i<=a/2;++i)
 {
 if(a%i==0)
 {
 flag=1;
 break;
-}}}
-if(flag==0)
-{
-c=i;
-printf("%d ",c);
 }}
+if(flag==0)
+printf("%d ",a);
+++a;
+}
+  return 0;
+}

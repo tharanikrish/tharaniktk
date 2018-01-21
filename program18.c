ea7277fc3e56ad261ef3c98fe20950a539228b02
tharanikrish <31505996+tharanikrish@users.noreply.github.com>
2018-01-21 12:26:39 +0530

    Update program18.c

--- program18.c
+++ program18.c
@@ -7,18 +7,16 @@ int main()
 	printf("%d%d",a,b);
 	for(i=a+1;i<b;i++)
 	{
-		t=i;
-		n=0;
-		while(t!=0)
+	t=i;
+	n=0;
+	while(t!=0)
 	{
-		rem=t%10;
-		n=n+rem*rem*rem;
-		t=t/10;
+	rem=t%10;
+	n=n+rem*rem*rem;
+	t=t/10;
 	}
 	if(i==n)
-	{
-		printf("%d",i);
-	}
+	printf("%d",i);
 	}
 		
 	return 0;

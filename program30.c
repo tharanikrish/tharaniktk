141c184b5eb266022f8a068fde8a106a89926f84
tharanikrish <31505996+tharanikrish@users.noreply.github.com>
2018-01-31 16:55:03 +0530

    Create program30.c

---
+++ program30.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+    int a,b,c,d,e,f;
+    printf("enter the first time");
+    scanf("%d%d",&a,&b);
+    printf("enter the second time");
+    scanf("5%",&c,&d);
+    e=a-c;
+    f=b-d;
+    printf("the time is%d%d",e,f);
+    return 0;
+}

7a9e5d74bf65e0f1f680c760c04ba7d9ddc4c08b
tharanikrish <31505996+tharanikrish@users.noreply.github.com>
2018-02-05 17:00:25 +0530

    Update program31.c

--- program31.c
+++ program31.c
@@ -6,3 +6,20 @@ int count_char=0;
 char str[26];
 printf("\n Enter the string:");
 scanf(
+
+  
+  
+  #include<stdio.h>
+int main()
+{
+int count_char=0;
+char str[20];
+printf("enter string:");
+gets(str);
+for(i=0;str[i]=!NULL;i++)
+{
+count_char++;
+}
+printf("\n number of characters in string:%d",count_char);
+return(0);
+}

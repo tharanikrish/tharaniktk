7e663763a0e65c6d11110900e2389fc6c682f620
tharanikrish <31505996+tharanikrish@users.noreply.github.com>
2018-02-05 16:47:03 +0530

    Create program28.c

---
+++ program28.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int i,n,a[n];
+printf("\n Enter the number:");
+scanf("%d",&n);
+printf("\n Enter the array elements:\n");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf(" %d %d \n",a[i],i);
+return 0;
+}

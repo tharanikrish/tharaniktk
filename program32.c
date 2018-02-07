9573e3e75ffc6141ee2a102734434f28e747cf44
tharanikrish <31505996+tharanikrish@users.noreply.github.com>
2018-02-07 16:34:56 +0530

    Create program32.c

---
+++ program32.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int count_char=0,i;
+char str[26];
+printf("\n Enter the string:");
+  scanf("%s",&str[i]);
+  for(i=0;str[i]=!NULL;i++)
+  {
+    if(
+  }
+  printf("\nNumber of characters in string:",count_char);
+return 0;
+  }
+  

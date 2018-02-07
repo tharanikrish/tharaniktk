0e26b88455a4e80dccf9b75dbc0785634484db2c
tharanikrish <31505996+tharanikrish@users.noreply.github.com>
2018-02-07 16:03:03 +0530

    Update program31.c

--- program31.c
+++ program31.c
@@ -3,9 +3,9 @@
 int main()
 {
 int count_char=0;
-char str[26];
+char str[26],i;
 printf("\n Enter the string:");
-  gets(str);
+  scanf("%s",&str[i]);
   for(i=0;str[i]=!NULL;i++)
   {
     count_char++;

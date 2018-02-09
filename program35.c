b280f88001b08eb14a22e76e368cf48ad29fa56f
tharanikrish <31505996+tharanikrish@users.noreply.github.com>
2018-02-09 16:55:08 +0530

    Create program35.c

---
+++ program35.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int i,len,line=0;
+char str[100];
+printf("\n Enter the string:");
+  scanf("%[\t\n]s",&str);
+  len=strlen(str);
+  for(i=0;i<=len;i++)
+  {
+    if(str[i]>='0' && str[i]<=9)
+      line++;
+  }
+  printf("\nNumber of numeric characters:%d",line);
+return 0;
+  }

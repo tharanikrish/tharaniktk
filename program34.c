7de3e2cc2109ab2e91b41987aaf57d45f5944665
tharanikrish <31505996+tharanikrish@users.noreply.github.com>
2018-02-09 16:49:33 +0530

    Update program34.c

--- program34.c
+++ program34.c
@@ -4,9 +4,9 @@ int main()
 int i,len,line=0;
 char str[100];
 printf("\n Enter the string:");
-  scanf("%[\n]s",&str);
-  len=strlen(a);
-  for(i=0;i<len;i++)
+  scanf("%s",&str);
+  len=strlen(str);
+  for(i=0;i<=len;i++)
   {
     if(str[i]=='.')
       line=line+1;

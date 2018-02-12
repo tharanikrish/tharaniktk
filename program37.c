02181319aba02d872688b4707bc4354d63d4c53d
tharanikrish <31505996+tharanikrish@users.noreply.github.com>
2018-02-12 16:38:51 +0530

    Update program37.c

--- program37.c
+++ program37.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-int a,b;
+int a,b,temp;
 printf("\nEnter the numbers:");
 scanf("%d%d",&a,&b);
 printf("\nBefore swapping:%d %d",a,b);
-a=a+b;
-b=a-b;
-a=b-a;
+temp=a;
+a=b;
+b=temp;
 printf("\nAfter swapping:%d %d",a,b);
 return 0;
 }

4921eb3bef1d1f3eb1d848bd0939424fa3c1067e
tharanikrish <31505996+tharanikrish@users.noreply.github.com>
2018-02-19 16:35:43 +0530

    Update program40.c

--- program40.c
+++ program40.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include<conio.h>
 int main()
 {
 int i,n,t1=0,t2=1,temp;
@@ -8,11 +7,10 @@ scanf("%d",&n);
 printf("\nfibanocci series:");
 for(i=1;i<=n;++i)
 {
-printf("%d",t1);
+printf(" %d",t1);
 temp=t1+t2;
 t1=t2;
 t2=temp;
 }
-getch();
 return 0;
 }

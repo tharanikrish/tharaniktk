b8f29896142467e52af4b7c9173911e16180c8e0
tharanikrish <31505996+tharanikrish@users.noreply.github.com>
2018-02-19 16:25:29 +0530

    Create program40.c

---
+++ program40.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,n,t1=0,t2=1,temp;
+printf("\nnumber of terms:");
+scanf("%d",&n);
+printf("\nfibanocci series:");
+for(i=1;i<=n;++i)
+{
+printf("%d",t1);
+temp=t1+t2;
+t1=t2;
+t2=temp;
+}
+getch();
+return 0;
+}

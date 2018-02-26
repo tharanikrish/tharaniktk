d43e7ad416ade6f106197face3bf6e672d44fc44
tharanikrish <31505996+tharanikrish@users.noreply.github.com>
2018-02-26 16:02:12 +0530

    Update program36.c

--- program36.c
+++ program36.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
-void main()
+#include<string.h>
+int main()
 {
 char str[30];
 int l,i,count=0;
 printf("\nEnter the string:");
 scanf("%s",&str);
 l=strlen(str);
-for(i=0;i<1;i++)
+for(i=0;i<l;i++)
 {
-if((str[i>=33)&&(str[i]<=47)||(str[i]>=58)&&(str[i]<=64)||(str[i]>=91)&&(str[i]<=96)||(str[i]>=123)&&(str[i]<=127))
+if((str[i]>=33)&&(str[i]<=47)||(str[i]>=58)&&(str[i]<=64)||(str[i]>=91)&&(str[i]<=96)||(str[i]>=123)&&(str[i]<=127))
 {
 count++;
-}
+}}
 printf("\nnumber of special characters:%d",count);
+return 0;
 }
